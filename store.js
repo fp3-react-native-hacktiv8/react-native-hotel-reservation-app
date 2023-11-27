8181e55da67fa8a114db076e45bdab6e4a15b33f
Ali Rosi <alirosi3214@gmail.com>
2023-11-28 06:45:37 +0700

    fix previous commit--

--- store.js
+++ store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import SavedReducer from "./SavedReducer";
 
 export default configureStore({
-    reducer:{
-        booking:SavedReducer
-    }
-})
+  reducer: {
+    booking: SavedReducer,
+  },
+});
